43b730239ae2c637ed01c52bd52a6f3b3204165a
Patryk Sztuczka <patryk@jarvis>
2024-10-25 09:13:29 +0200

    Created method for extracting mp3 track from mp4 file

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,34 @@
+use std::process::Command;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command(rename_all = "snake_case")]
+fn extract_audio(file_path: &str) {
+    println!("File path: {}", file_path);
+    let output = Command::new("ffmpeg")
+        .arg("-i")
+        .arg(file_path)
+        .arg("-vn")
+        .arg("-y")
+        .arg("output.mp3")
+        .spawn()
+        .expect("ffmpeg command failed to start");
+
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, extract_audio])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
