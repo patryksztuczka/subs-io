a47060a4cc4d100176bd37ae68028f36b2735aea
Patryk Sztuczka <patryk@jarvis>
2024-11-04 17:44:01 +0100

    Improved audio extracting function a bit

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -9,18 +10,27 @@ fn greet(name: &str) -> String {
 #[tauri::command(rename_all = "snake_case")]
 fn extract_audio(file_path: &str) {
     println!("File path: {}", file_path);
-    let output = Command::new("ffmpeg")
+    let start = SystemTime::now();
+
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_millis();
+
+    let output_file = format!("./audio-tracks/audio-{}.mp3", since_the_epoch);
+
+    let _output = Command::new("ffmpeg")
         .arg("-i")
         .arg(file_path)
         .arg("-vn")
         .arg("-y")
-        .arg("output.mp3")
+        .arg(output_file)
         .spawn()
         .expect("ffmpeg command failed to start");
 
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    // println!("status: {}", output.status);
+    // println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    // println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
